63ab57b94ae8a8f334adf3073c958156b707ec72
pegoudal <pierre.eric.goudal@gmail.com>
2017-02-24 15:46:42 +0100

    Modification de cours.js, passage en mode asynchrone

--- js/cours.js
+++ js/cours.js
@@ -1,8 +1,9 @@
-// Création d'une requête HTTP
 var req = new XMLHttpRequest();
-// Requête HTTP GET synchrone vers le fichier langages.txt publié localement
-req.open("GET", "http://localhost/javascript-web-srv/data/langages.txt", false);
-// Envoi de la requête
+// La requête est asynchrone lorsque le 3ème paramètre vaut true ou est absent
+req.open("GET", "http://localhost/javascript-web-srv/data/langages.txt");
+// Gestion de l'événement indiquant la fin de la requête
+req.addEventListener("load", function () {
+    // Affiche la réponse reçue pour la requête
+    console.log(req.responseText);
+});
 req.send(null);
-// Affiche la réponse reçue pour la requête
-console.log(req.responseText);
